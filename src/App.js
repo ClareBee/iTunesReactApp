75524f5f05da558778179ca49ebd32f4207a00ab
Clare <chblackburne@gmail.com>
2018-01-09 17:37:42 +0000

    Adds ListContainer and links with app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import ListContainer from './containers/ListContainer';
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-
+      <ListContainer />
     );
   }
 }
---
+++ src/containers/ListContainer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class ListContainer extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return(
+      <h1>This is the list container</h1>
+    );
+  }
+}
+
+export default ListContainer;
