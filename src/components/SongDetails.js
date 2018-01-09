89c1bbb6274b427e42cc2e9955ddeafd62ea85b8
Clare <chblackburne@gmail.com>
2018-01-09 19:36:50 +0000

    Adds image and price to song view

--- src/components/SongDetails.js
+++ src/components/SongDetails.js
@@ -6,14 +6,19 @@ class SongDetails extends React.Component{
     if(!this.props.song){
     return null
   }
+    let individual = this.props.song;
+    console.log(this.props.song);
+    console.log(this.props.song["im:image"][0].label);
+    var bla = Object.values(individual);
+    console.log(bla[1]);
+
     return(
       <article id="country-detail">
         <h3>
       Song: {this.props.song.title.label}
       </h3>
-      <h4>
-  
-        </h4>
+      <img src={this.props.song['im:image'][0].label} />
+      <h1>{this.props.song['im:price'].label}</h1>
       </article>
     );
   }
