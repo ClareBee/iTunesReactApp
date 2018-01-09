4b02fcbe515294249b51b69a03a59aaa18f9791a
Clare <chblackburne@gmail.com>
2018-01-09 20:27:52 +0000

    Adds bootstrap functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import ListContainer from './containers/ListContainer';
-
+import Button from './components/Buttons';
+import Jumbotron1 from './components/Jumbotron';
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
+      <div>
+        <Jumbotron1 />
       <ListContainer />
+      <Button />
+    </div>
     );
   }
 }
---
+++ src/components/Buttons.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Button from 'react-bootstrap/lib/Button';
+
+
+class Buttons extends React.Component {
+  render(){
+    return(
+  <Button bsStyle="primary" bsSize="large" active>
+    Primary button
+  </Button>
+);
+  }
+}
+
+export default Buttons;
